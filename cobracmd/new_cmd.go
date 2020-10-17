45fa003952f826e69336fe0becbb6f27d234d3ca
Aleksey Salnikov <me@iamsalnikov.ru>
2020-10-17 13:53:52 +0300

    NewListCmd

---
+++ cobracmd/new_cmd.go
@@ -0,0 +1,35 @@
+package cobracmd
+
+import (
+	"fmt"
+
+	"github.com/iamsalnikov/mymigrate"
+	"github.com/spf13/cobra"
+)
+
+// NewListCmd is a cobra command that prints list of applied migrations
+var NewListCmd = &cobra.Command{
+	Use:   "new-list",
+	Short: "shows list of new migrations",
+	RunE:  NewListRunE,
+}
+
+// NewListRunE is a cobra run function for NewListCmd command
+func NewListRunE(cmd *cobra.Command, args []string) error {
+	list, err := mymigrate.History()
+	if err != nil {
+		return err
+	}
+
+	if len(list) == 0 {
+		_, _ = fmt.Fprintln(cmd.OutOrStdout(), "There are no new migrations")
+
+		return nil
+	}
+
+	for _, mig := range list {
+		_, _ = fmt.Fprintln(cmd.OutOrStdout(), mig)
+	}
+
+	return nil
+}
